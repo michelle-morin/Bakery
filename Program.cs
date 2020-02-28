830b179d62fdb032feeeab4ceef0b8646d468ef8
Michelle Morin <morin2m@gmail.com>
2020-02-28 10:01:52 -0800

    add exception handling for when user does not input numerical quantity

--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ namespace Bakery
 
     public static string InitializeOrder()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery! We sell bread for $5/loaf or $10/three loaves, and also sell pastries for $2/each or $5/three pastries.");
+      Console.BackgroundColor = ConsoleColor.Cyan;
+      Console.ForegroundColor = ConsoleColor.Black;
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Bread is $5/loaf or $10/three loaves; pastries are $2/each or $5/three pastries.");
       Console.WriteLine("Would you like to purchase bread, pastry, or both?");
       Console.WriteLine("[BREAD] [PASTRY] [BOTH]");
       string bakerySelection = Console.ReadLine();
@@ -25,20 +28,45 @@ namespace Bakery
       if (selection.ToLower() == "bread")
       {
         int breadOrderTotal = OrderBread();
-        Console.WriteLine("Your total will be $" + breadOrderTotal);
-
+        if (breadOrderTotal > 0)
+        {
+          Console.WriteLine("Your total will be $" + breadOrderTotal);
+        }
+        else
+        {
+          AskToReturn();
+        }
       }
       else if (selection.ToLower() == "pastry")
       {
         int pastryOrderTotal = OrderPastry();
-        Console.WriteLine("Your total will be $" + pastryOrderTotal);
+        if (pastryOrderTotal > 0)
+        {
+          Console.WriteLine("Your total will be $" + pastryOrderTotal);
+        }
+        else
+        {
+          AskToReturn();
+        }
       }
       else if (selection.ToLower() == "both")
       {
         int breadTotal = OrderBread();
         int pastryTotal = OrderPastry();
         int comboOrderTotal = breadTotal + pastryTotal;
-        Console.WriteLine("Your total will be $" + comboOrderTotal);
+        if (breadTotal >= 0 && pastryTotal >= 0 && comboOrderTotal > 0)
+        {
+          Console.WriteLine("Your total will be $" + comboOrderTotal);
+        }
+        else if (breadTotal == 0 && pastryTotal == 0)
+        {
+          Console.WriteLine("You did not select any bread or pastries to purchase.");
+          AskToReturn();
+        }
+        else
+        {
+          AskToReturn();
+        }
       }
       else
       {
@@ -50,22 +78,74 @@ namespace Bakery
     {
       Console.WriteLine("Please enter a quantity of bread loaves you would like to purchase:");
       string stringBreadQuantity = Console.ReadLine();
-      int breadQuantity = int.Parse(stringBreadQuantity);
-      Bread breadOrder = new Bread(breadQuantity);
-      breadOrder.ApplyBreadDeals();
-      int breadOrderTotal = breadOrder.Price;
-      return breadOrderTotal;
+      int breadQuantity = ValidateInputQuantity(stringBreadQuantity);
+      if (breadQuantity > 0)
+      {
+        Bread breadOrder = new Bread(breadQuantity);
+        breadOrder.ApplyBreadDeals();
+        int breadOrderTotal = breadOrder.Price;
+        return breadOrderTotal;
+      }
+      else
+      {
+        return 0;
+      }
     }
-    
+
     public static int OrderPastry()
     {
       Console.WriteLine("Please enter a quantity of pastries you would like to purchase:");
       string stringPastryQuantity = Console.ReadLine();
-      int pastryQuantity = int.Parse(stringPastryQuantity);
-      Pastry pastryOrder = new Pastry(pastryQuantity);
-      pastryOrder.ApplyPastryDeals();
-      int pastryOrderTotal = pastryOrder.Price;
-      return pastryOrderTotal;
+      int pastryQuantity = ValidateInputQuantity(stringPastryQuantity);
+      if (pastryQuantity >= 0)
+      {
+        Pastry pastryOrder = new Pastry(pastryQuantity);
+        pastryOrder.ApplyPastryDeals();
+        int pastryOrderTotal = pastryOrder.Price;
+        return pastryOrderTotal;
+      }
+      else
+      {
+        return 0;
+      }
+    }
+
+    public static int ValidateInputQuantity(string quantity)
+    {
+      try
+      {
+        int intQuantity = int.Parse(quantity);
+        return intQuantity;
+      }
+      catch (FormatException)
+      {
+        Console.WriteLine("The quantity entered is not valid");
+        return -1;
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine("Unexpected error: " + exception.Message);
+        return -1;
+      }
+    }
+
+    public static void AskToReturn()
+    {
+      Console.WriteLine("Would you like to return to the main menu?");
+      Console.WriteLine("[YES] or [NO]");
+      string returnToMenu = Console.ReadLine();
+      if (returnToMenu.ToLower() == "yes" || returnToMenu.ToLower() == "y")
+      {
+        Main();
+      }
+      else if (returnToMenu.ToLower() == "no" || returnToMenu.ToLower() == "n")
+      {
+        Console.WriteLine("Come again soon!");
+      }
+      else
+      {
+        AskToReturn();
+      }
     }
   }
 }
