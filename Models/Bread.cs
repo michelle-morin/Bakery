686b4b8bc52b37d11391629ef5e9371c0c7937b4
Michelle Morin <morin2m@gmail.com>
2020-02-28 12:15:24 -0800

    add static property totalQuantity and update methods for calculating deals on single order and deals on all orders

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -9,12 +9,14 @@ namespace Bakery.Models
     public int Price { get; set; }
     private static List<Bread> _ordersOfBread = new List<Bread>() {};
     private static int _totalPrice;
+    private static int _totalQuantity;
     
     public Bread(int quantity)
     {
       Quantity = quantity;
       Price = 5 * quantity;
       _ordersOfBread.Add(this);
+      _totalQuantity += quantity;
     }
 
     public static List<Bread> GetThatBread()
@@ -27,26 +29,45 @@ namespace Bakery.Models
       return _totalPrice;
     }
 
+    public static int GetQuantity()
+    {
+      return _totalQuantity;
+    }
+
+    public void ApplyDealsToSingleOrder()
+    {
+      if (Quantity % 3 == 0)
+      {
+        Price = (Quantity / 3) * 10;
+      }
+      else if (Quantity >= 3)
+      {
+        int remainder = Quantity % 3;
+        int quotient = Quantity / 3;
+        Price = (remainder * 5) + (quotient * 10);
+      }
+    }
+
     public static void ApplyBreadDeals()
     {
-      int totalQuantity = 0;
-      foreach(Bread breadOrder in _ordersOfBread)
+      // int totalQuantity = 0;
+      // foreach(Bread breadOrder in _ordersOfBread)
+      // {
+      //   totalQuantity += breadOrder.Quantity;
+      // }
+      if (_totalQuantity % 3 == 0)
       {
-        totalQuantity += breadOrder.Quantity;
+        _totalPrice = (_totalQuantity / 3) * 10;
       }
-      if (totalQuantity % 3 == 0)
+      else if (_totalQuantity >= 3)
       {
-        _totalPrice = (totalQuantity / 3) * 10;
-      }
-      else if (totalQuantity >= 3)
-      {
-        int remainder = totalQuantity % 3;
-        int quotient = totalQuantity / 3;
+        int remainder = _totalQuantity % 3;
+        int quotient = _totalQuantity / 3;
         _totalPrice = (remainder * 5) + (quotient * 10);
       }
       else
       {
-        _totalPrice = 5 * totalQuantity;
+        _totalPrice = 5 * _totalQuantity;
       }
     }
   }
--- Models/Pastry.cs
+++ Models/Pastry.cs
@@ -9,12 +9,14 @@ namespace Bakery.Models
     public int Price { get; set; }
     private static List<Pastry> _ordersOfPastries = new List<Pastry>() {};
     private static int _totalPrice;
+    private static int _totalQuantity;
     
     public Pastry(int quantity)
     {
       Quantity = quantity;
       Price = 2 * quantity;
       _ordersOfPastries.Add(this);
+      _totalQuantity += quantity;
     }
 
     public static List<Pastry> GetThePastries()
@@ -27,22 +29,41 @@ namespace Bakery.Models
       return _totalPrice;
     }
 
+    public static int GetQuantity()
+    {
+      return _totalQuantity;
+    }
+
+    public void ApplyDealsToSingleOrder()
+    {
+      if (Quantity >= 3)
+      {
+        int remainder = Quantity % 3;
+        int quotient = Quantity / 3;
+        Price = (remainder * 2) + (quotient * 5);
+      }
+      else
+      {
+        Price = 2 * Quantity;
+      }
+    }
+
     public static void ApplyPastryDeals()
     {
-      int totalQuantity = 0;
-      foreach(Pastry pastryOrder in _ordersOfPastries)
+      // int totalQuantity = 0;
+      // foreach(Pastry pastryOrder in _ordersOfPastries)
+      // {
+      //   totalQuantity += pastryOrder.Quantity;
+      // }
+      if (_totalQuantity >= 3)
       {
-        totalQuantity += pastryOrder.Quantity;
-      }
-      if (totalQuantity >= 3)
-      {
-        int remainder = totalQuantity % 3;
-        int quotient = totalQuantity / 3;
+        int remainder = _totalQuantity % 3;
+        int quotient = _totalQuantity / 3;
         _totalPrice = (remainder * 2) + (quotient * 5);
       }
       else
       {
-        _totalPrice = 2 * totalQuantity;
+        _totalPrice = 2 * _totalQuantity;
       }
     }
   }
